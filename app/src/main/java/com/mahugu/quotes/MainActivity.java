44746592db20cbc7307b69f0af79d7fbe2e6f3a5
Bihengoto <isaacbihengoto@gmail.com>
2019-08-25 01:01:32 +0300

    included the code for toast in the mainactivity

--- app/src/main/java/com/mahugu/quotes/MainActivity.java
+++ app/src/main/java/com/mahugu/quotes/MainActivity.java
@@ -2,12 +2,25 @@ package com.mahugu.quotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mbutton = (Button) findViewById(R.id.button);
+
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG);
+            }
+        });
     }
 }
