f1c6228b79355ec4b3f0447e859a119d1de67ab4
Bihengoto <isaacbihengoto@gmail.com>
2019-08-25 21:56:38 +0300

    implemented the interface on the mainactivity

--- app/src/main/java/com/mahugu/quotes/MainActivity.java
+++ app/src/main/java/com/mahugu/quotes/MainActivity.java
@@ -3,26 +3,47 @@ package com.mahugu.quotes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button mbutton;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.button) Button mbutton;
+    @BindView(R.id.quoteEditText) EditText mQuoteEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+//        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+//        Typeface opensans = Typeface.createFromAsset(getAssets(), "fonts/opensans_regular.ttf");
+//        mAppNameTextView.setTypeface(opensans);
+
+        mQuoteEditText = (EditText) findViewById(R.id.quoteEditText);
         mbutton = (Button) findViewById(R.id.button);
 
-        mbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, QuotesActivity.class);
-                startActivity(intent);
-            }
-        });
+        mbutton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mbutton) {
+            String location = mQuoteEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, QuotesActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
