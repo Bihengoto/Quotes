aa26795a0994ccde665b08ec09d86d61c7100e4f
Bihengoto <isaacbihengoto@gmail.com>
2019-09-03 15:46:27 +0300

    created and added code for the link in constant java file

---
+++ app/src/main/java/com/mahugu/quotes/Constants.java
@@ -0,0 +1,7 @@
+package com.mahugu.quotes;
+
+public class Constants {
+
+    public static final String QUOTE_BASE_URL = "http://quotes.stormconsultancy.co.uk/popular.json";
+    public static final String QUOTE_AUTHOR_QUERY_PARAMETER = "id";
+}
